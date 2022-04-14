b4682fe7131776fd6ccd9bb1d0580ee921de0907
morozov.da <morozov.da@gmail.com>
2022-04-14 22:02:54 +0300

    Patterns_WSGI Lesson_4 _ logger

--- Lesson_4/views.py
+++ Lesson_4/views.py
@@ -3,7 +3,9 @@ from mini_framework.request import RequestWSGI
 from mini_framework.respose import ResponseWSGI
 from mini_framework.variables import RESPONSE_200, RESPONSE_404
 from mini_framework.page_controller import page_controller, render_template
+from mini_framework.logger import LoggerWsgi
 
+logger = LoggerWsgi()
 
 class Index:
 	# print('---- my view ----')
@@ -16,7 +18,8 @@ class Index:
 		}
 		# print(self, context, page, path, page['template_name'], '------------ my view ----------')
 		html_render = render_template(templates_path=path, template_name=page['template_name'], context=context)
-		print(html_render, '------------ my view ----------')
+		logger.get_logger(message=f"{html_render}", file_log=False)
+		# print(html_render, '------------ my view ----------')
 		return html_render
 
 
@@ -42,9 +45,8 @@ class Contacts:
 			'text': 'test text',
 			'message': None
 		}
-		# 'message': None
-		print(page, path, request, '-------- Contacts.run() args --------')
 
+		logger.get_logger(message=f"{page, path, request}", file_log=False)
 		try:
 			if request.method == 'POST':
 				Contacts.post_request(self, request)
@@ -61,11 +63,12 @@ class Contacts:
 		self.context.update({'message': 'Ваше сообщение успешно отправлено'})
 		# print(request.wsgi_input_data)
 		input_message = request.wsgi_input_data
-		print(input_message)
-		current_day = time.strftime("%Y-%m-%d--", time.localtime())
-		current_time = time.strftime("%Y-%m-%d-%H.%M.%S -- ", time.localtime())
-		with open(f'messages/{current_day}messages.txt', 'a', encoding='utf-8') as messages_file:
-			messages_file.write(f"{current_time} Message\n"
+		# print(input_message)
+		logger.get_logger(message=f"{input_message}")
+		current_time = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+		current_day = current_time[:-9:]
+		with open(f'messages/{current_day}--messages.txt', 'a', encoding='utf-8') as messages_file:
+			messages_file.write(f"{current_time} -- Message\n"
 								f" 	Email: {''.join(input_message['email'])},\n"
 								f" 	Title: {''.join(input_message['title'])},\n"
 								f"	Text: {''.join(input_message['text'])}. \n")
