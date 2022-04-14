b60ecc481c4d3d844dcb4ea9919a5bd651fac158
morozov.da <morozov.da@gmail.com>
2022-04-14 14:45:36 +0300

    Patterns_WSGI Lesson_4 _ write message to file

--- Lesson_4/views.py
+++ Lesson_4/views.py
@@ -9,7 +9,7 @@ class Index:
 	print('---- my view ----')
 
 	def run(self, page, path, request):
-		print(page, path, request,  '-------- Index.run() args --------')
+		print(page, path, request, '-------- Index.run() args --------')
 		context = {
 			'page_name': 'Index - my view',
 			'text': 'test text'
@@ -23,7 +23,6 @@ class Index:
 class About:
 
 	def run(self, page, path, request):
-
 		# print(page, path, '-------- About.run() args --------')
 		context = {
 			'page_name': 'About - my view',
@@ -38,35 +37,39 @@ class About:
 class Contacts:
 
 	def run(self, page, path, request):
-		context = {
+		self.context = {
 			'page_name': 'Contact - my view',
 			'text': 'test text',
 			'message': None
 		}
 		# 'message': None
-		print(page, path, request,  '-------- Contacts.run() args --------')
+		print(page, path, request, '-------- Contacts.run() args --------')
 		# print(page, path, '-------- About.run() args --------')
 
 		try:
 			if request.method == 'POST':
-				context.update({'message': 'Ваше сообщение успешно отправлено'})
-				# print(request.wsgi_input_data)
-				input_message = request.wsgi_input_data
-				print(input_message)
-				current_day = time.strftime("%Y-%m-%d--", time.localtime())
-				current_time = time.strftime("%Y-%m-%d-%H.%M.%S -- ", time.localtime())
-				with open(f'messages/{current_day}messages.txt', 'a', encoding='utf-8') as messages_file:
-					messages_file.write(f"{current_time} Message\n"
-										f" 	Email: {''.join(input_message['email'])},\n"
-										f" 	Title: {''.join(input_message['title'])},\n"
-										f"	Text: {''.join(input_message['text'])}. \n")
+				Contacts.post_request(self, request)
 		except:
 			pass
 
 		# print(self, context, page, path, page['template_name'], '------------ my view ----------')
-		html_render = render_template(templates_path=path, template_name=page['template_name'], context=context)
+		html_render = render_template(templates_path=path, template_name=page['template_name'], context=self.context)
 		# print(html_render, '------------ my view ----------')
 		return html_render
+
+	def post_request(self, request):
+
+		self.context.update({'message': 'Ваше сообщение успешно отправлено'})
+		# print(request.wsgi_input_data)
+		input_message = request.wsgi_input_data
+		print(input_message)
+		current_day = time.strftime("%Y-%m-%d--", time.localtime())
+		current_time = time.strftime("%Y-%m-%d-%H.%M.%S -- ", time.localtime())
+		with open(f'messages/{current_day}messages.txt', 'a', encoding='utf-8') as messages_file:
+			messages_file.write(f"{current_time} Message\n"
+								f" 	Email: {''.join(input_message['email'])},\n"
+								f" 	Title: {''.join(input_message['title'])},\n"
+								f"	Text: {''.join(input_message['text'])}. \n")
 
 
 if __name__ == '__main__':
